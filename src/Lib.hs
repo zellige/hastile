3fe10859da313c64fbd89651a7076e2b579d8d66
Andrew McCluskey <andrew@ajmccluskey.com>
2016-09-13 09:13:14 +1000

    Add layer capture

--- src/Lib.hs
+++ src/Lib.hs
@@ -16,11 +16,12 @@ import           Servant
 
 import           SphericalMercator
 
-type Z = Capture "z" Integer
-type X = Capture "x" Integer
-type Y = Capture "y" Integer
-type HastileApi =    Z :> X :> Y :> "query" :> Get '[PlainText] Text
-                :<|> Z :> X :> Y :> "mvt" :> Get '[PlainText] Text
+type Layer = Capture "layer" String
+type Z     = Capture "z" Integer
+type X     = Capture "x" Integer
+type Y     = Capture "y" Integer
+type HastileApi =    Layer :> Z :> X :> Y :> "query" :> Get '[PlainText] Text
+                :<|> Layer :> Z :> X :> Y :> "mvt" :> Get '[PlainText] Text
 
 api :: Proxy HastileApi
 api = Proxy
@@ -30,8 +31,8 @@ hastileService pool =
   enter (runReaderTNat pool) (getQuery :<|> getTile)
 
 
-getQuery :: MonadReader P.Pool m => Integer -> Integer -> Integer -> m Text
-getQuery z x y = pure $ Data.Text.concat [queryPre, bboxSql, queryPost]
+getQuery :: MonadReader P.Pool m => String -> Integer -> Integer -> Integer -> m Text
+getQuery _l z x y = pure $ Data.Text.concat [queryPre, bboxSql, queryPost]
   where queryPre  = "SELECT linkid, name, load_total_24_2way, load_commveh_24_2way, \
                        \ST_AsGeoJSON(wkb_geometry) as the_geom_geojson FROM \"traffic_per_v5b\" \
                      \WHERE ST_Intersects(wkb_geometry, "
@@ -42,5 +43,5 @@ getQuery z x y = pure $ Data.Text.concat [queryPre, bboxSql, queryPost]
                            \ST_MakePoint(" ++ show llX ++ ", " ++ show llY ++ ", \
                            \ST_MakePoint(" ++ show urX ++ ", " ++ show urY ++ ")) 3857) 4326)"
 
-getTile :: Applicative m => Integer -> Integer -> Integer -> m Text
-getTile _z _x _y = pure "I'm not implemented yet"
+getTile :: Applicative m => String -> Integer -> Integer -> Integer -> m Text
+getTile _l _z _x _y = pure "I'm not implemented yet"
