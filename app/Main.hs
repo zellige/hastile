1a6ddbe09d778c6904753ece716c2e20a17594ee
Andrew McCluskey <andrew@ajmccluskey.com>
2016-09-13 10:00:42 +1000

    Add layers to server state
    Add ServerState to contain postgres connection pool and layer
    configuration. Layer config is a map from layer names to queries used to
    pull out features from PostGIS. Add escaping of `!bbox4326!` in queries,
    and replace it with SQL for bounding box in EPSG4326.

--- app/Main.hs
+++ app/Main.hs
@@ -7,13 +7,15 @@ import           Control.Exception.Base
 import           Data.Aeson
 import qualified Data.ByteString.Char8       as BS
 import qualified Data.ByteString.Lazy.Char8  as BSL
+import           Data.Map
 import           Data.Maybe (fromMaybe)
 import           Data.Time (NominalDiffTime)
 import           Hasql.Pool                  as P
-import           Lib
 import qualified Network.Wai.Handler.Warp    as Warp
 import           Options.Generic
 import           Servant
+
+import           Lib
 
 data CmdLine = CmdLine { configFile :: FilePath
                        }
@@ -24,6 +26,7 @@ data Config = Config { pgConnection :: String
                      , port :: Maybe Int
                      , pgPoolSize :: Maybe Int
                      , pgTimeout :: Maybe NominalDiffTime
+                     , layers' :: Map String String
                      } deriving (Show, Generic)
 instance FromJSON Config where
 
@@ -41,7 +44,8 @@ doItWithConfig :: Config -> IO ()
 doItWithConfig config = do
   let pgPoolSize' = fromMaybe 10 $ pgPoolSize config
       pgTimeout'  = fromMaybe 1 $ pgTimeout config
-      port'    = fromMaybe 8080 $ port config
+      port'       = fromMaybe 8080 $ port config
+      layers''    = layers' config
   bracket (P.acquire (pgPoolSize', pgTimeout', BS.pack . pgConnection $ config))
           (P.release)
-          (\pool -> Warp.run port'. serve api $ hastileService pool)
+          (\p -> Warp.run port'. serve api $ hastileService (ServerState p layers''))
--- src/Lib.hs
+++ src/Lib.hs
@@ -7,10 +7,12 @@
 module Lib
     ( api
     , hastileService
+    , ServerState (..)
     ) where
 
 import           Control.Monad.Reader.Class
-import           Data.Text
+import           Data.Map
+import           Data.Text                  as T
 import           Hasql.Pool                 as P
 import           Servant
 
@@ -23,25 +25,33 @@ type Y     = Capture "y" Integer
 type HastileApi =    Layer :> Z :> X :> Y :> "query" :> Get '[PlainText] Text
                 :<|> Layer :> Z :> X :> Y :> "mvt" :> Get '[PlainText] Text
 
+data ServerState = ServerState { pool :: P.Pool
+                               , layers :: Map String String
+                               }
+
 api :: Proxy HastileApi
 api = Proxy
 
-hastileService :: Pool -> Server HastileApi
-hastileService pool =
-  enter (runReaderTNat pool) (getQuery :<|> getTile)
+hastileService :: ServerState -> Server HastileApi
+hastileService state =
+  enter (runReaderTNat state) (getQuery :<|> getTile)
 
 
-getQuery :: MonadReader P.Pool m => String -> Integer -> Integer -> Integer -> m Text
-getQuery _l z x y = pure $ Data.Text.concat [queryPre, bboxSql, queryPost]
-  where queryPre  = "SELECT linkid, name, load_total_24_2way, load_commveh_24_2way, \
+getQuery :: MonadReader ServerState m => String -> Integer -> Integer -> Integer -> m Text
+getQuery _l z x y = pure . escape bbox4326 $ query
+  where query = "SELECT linkid, name, load_total_24_2way, load_commveh_24_2way, \
                        \ST_AsGeoJSON(wkb_geometry) as the_geom_geojson FROM \"traffic_per_v5b\" \
-                     \WHERE ST_Intersects(wkb_geometry, "
-        queryPost = pack ")"
+                 \WHERE ST_Intersects(wkb_geometry, !bbox4326!)"
         (BBox (Metres llX) (Metres llY) (Metres urX) (Metres urY)) =
           googleToBBoxM 256 (ZoomLevel z) (GoogleTileCoords x y)
-        bboxSql   = pack $ "ST_Transform(ST_SetSRID(ST_MakeBox2D(\
+        bbox4326 = pack $ "ST_Transform(ST_SetSRID(ST_MakeBox2D(\
                            \ST_MakePoint(" ++ show llX ++ ", " ++ show llY ++ ", \
                            \ST_MakePoint(" ++ show urX ++ ", " ++ show urY ++ ")) 3857) 4326)"
 
 getTile :: Applicative m => String -> Integer -> Integer -> Integer -> m Text
 getTile _l _z _x _y = pure "I'm not implemented yet"
+
+escape :: Text -> Text -> Text
+escape bbox query = T.concat . fmap replace' . T.split (== '!') $ query
+  where replace' "bbox4326" = bbox
+        replace' t = t
