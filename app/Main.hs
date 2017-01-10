debe775176804afbc5b771b7b0e6a3323eb8b7d3
Andrew Newman <andrewfnewman@gmail.com>
2017-01-10 11:11:38 +1000

    Use CORS

--- app/Main.hs
+++ app/Main.hs
@@ -5,13 +5,14 @@ module Main where
 
 import           Control.Exception.Base
 import           Data.Aeson
-import qualified Data.ByteString.Char8      as BS
-import qualified Data.ByteString.Lazy.Char8 as BSL
+import qualified Data.ByteString.Char8       as BS
+import qualified Data.ByteString.Lazy.Char8  as BSL
 import           Data.Map
-import           Data.Maybe                 (fromMaybe)
-import           Data.Time                  (NominalDiffTime)
-import           Hasql.Pool                 as P
-import qualified Network.Wai.Handler.Warp   as Warp
+import           Data.Maybe                  (fromMaybe)
+import           Data.Time                   (NominalDiffTime)
+import           Hasql.Pool                  as P
+import qualified Network.Wai.Handler.Warp    as Warp
+import           Network.Wai.Middleware.Cors
 import           Options.Generic
 import           Servant
 
@@ -41,11 +42,14 @@ doIt cmdLine = do
     Nothing -> putStrLn $ "Failed to parse config file " ++ configFile cmdLine
 
 doItWithConfig :: Config -> IO ()
-doItWithConfig config = do
+doItWithConfig config =
   let pgPoolSize' = fromMaybe 10 $ pgPoolSize config
       pgTimeout'  = fromMaybe 1 $ pgTimeout config
       port'       = fromMaybe 8080 $ port config
       layers'    = layers config
-  bracket (P.acquire (pgPoolSize', pgTimeout', BS.pack . pgConnection $ config))
-          (P.release)
-          (\p -> Warp.run port'. serve api $ hastileService (ServerState p layers'))
+  in bracket (P.acquire (pgPoolSize', pgTimeout', BS.pack . pgConnection $ config))
+          P.release $
+     \p ->
+       Warp.run port' . cors (const $ Just policy) . serve api $ hastileService (ServerState p layers')
+         where
+           policy = simpleCorsResourcePolicy { corsRequestHeaders = ["Content-Type"] }
