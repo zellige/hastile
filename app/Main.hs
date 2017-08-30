83dce29ab4b5bca95cda5f01b0b9638bed739ddb
Andrew Newman <andrewfnewman@gmail.com>
2017-08-30 11:36:08 +1000

    Added some qualified imports

--- app/Main.hs
+++ app/Main.hs
@@ -3,24 +3,23 @@
 
 module Main where
 
-import           Control.Exception.Base
-import           Data.Aeson
+import qualified Control.Exception.Base      as CE
+import qualified Data.Aeson                  as A
 import qualified Data.ByteString.Lazy.Char8  as LBS
-import           Data.Foldable
-import           Data.Map
+import qualified Data.Foldable               as F
+import qualified Data.Map                    as M
 import           Data.Maybe                  (fromMaybe)
 import           Data.Monoid
-import           Data.Text
 import           Data.Text.Encoding
 import           GHC.Conc
-import           Hasql.Pool                  as P
+import qualified Hasql.Pool                  as P
 import           Network.Wai
 import qualified Network.Wai.Handler.Warp    as Warp
 import           Network.Wai.Middleware.Cors
 import           Options.Generic
-import           Servant
+import qualified Servant                     as S
 import           STMContainers.Map           as STM
-import           System.Exit
+import qualified System.Exit                 as SE
 
 import           Lib
 import           Types
@@ -32,19 +31,19 @@ doIt :: CmdLine -> IO ()
 doIt cmdLine = do
   let cfgFile = configFile cmdLine
   configBs <- LBS.readFile cfgFile
-  case eitherDecode configBs of
+  case A.eitherDecode configBs of
     Left e -> do
       putStrLn $ "In file: " <> cfgFile <> "\nError: " <> e
-      exitWith (ExitFailure 2)
+      SE.exitWith (SE.ExitFailure 2)
     Right inputConfig -> doItWithConfig cfgFile $ addDefaults inputConfig
 
 doItWithConfig :: FilePath -> Config -> IO ()
 doItWithConfig cfgFile config@Config{..} = do
   layers <- atomically STM.new :: IO (STM.Map Text Layer)
-  forM_ (Data.Map.toList _configLayers) $ \(k, v) -> atomically $ STM.insert v k layers
-  bracket (P.acquire (_configPgPoolSize, _configPgTimeout, encodeUtf8 _configPgConnection))
+  F.forM_ (M.toList _configLayers) $ \(k, v) -> atomically $ STM.insert v k layers
+  CE.bracket (P.acquire (_configPgPoolSize, _configPgTimeout, encodeUtf8 _configPgConnection))
     P.release $
-      \p -> getWarp _configPort . serve api $ hastileService (ServerState p _configMapnikInputPlugins cfgFile config layers)
+      \p -> getWarp _configPort . S.serve api $ hastileService (ServerState p _configMapnikInputPlugins cfgFile config layers)
   pure ()
 
 getWarp :: Warp.Port -> Network.Wai.Application -> IO ()
