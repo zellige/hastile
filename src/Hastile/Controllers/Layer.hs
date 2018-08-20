2010d36365755e6ac42caa134bece928b190e3ae
newmana <andrewfnewman@gmail.com>
2018-08-20 10:36:58 +1000

    Fix up some imports

--- src/Hastile/Controllers/Layer.hs
+++ src/Hastile/Controllers/Layer.hs
@@ -27,10 +27,10 @@ import           GHC.Conc
 import           ListT
 import           Network.HTTP.Types.Header  (hLastModified)
 import           Numeric.Natural            (Natural)
-import qualified Servant                    as S
-import qualified STMContainers.Map          as STM
+import qualified Servant                    as Servant
+import qualified STMContainers.Map          as STMMap
 
-import qualified Hastile.DB.Layer           as DB
+import qualified Hastile.DB.Layer           as DBLayer
 import qualified Hastile.Routes             as Routes
 import qualified Hastile.Tile               as Tile
 import qualified Hastile.Types.App          as App
@@ -38,24 +38,24 @@ import qualified Hastile.Types.Config       as Config
 import qualified Hastile.Types.Layer        as Layer
 
 
-layerServer :: S.ServerT Routes.LayerApi App.ActionHandler
-layerServer l = provisionLayer l S.:<|> coordsServer l
+layerServer :: Servant.ServerT Routes.LayerApi App.ActionHandler
+layerServer l = provisionLayer l Servant.:<|> coordsServer l
 
-coordsServer :: T.Text -> Natural -> Natural -> S.ServerT Routes.HastileContentApi App.ActionHandler
-coordsServer l z x = getQuery l z x S.:<|> getContent l z x
+coordsServer :: T.Text -> Natural -> Natural -> Servant.ServerT Routes.HastileContentApi App.ActionHandler
+coordsServer l z x = getQuery l z x Servant.:<|> getContent l z x
 
-stmMapToList :: STM.Map k v -> STM [(k, v)]
-stmMapToList = ListT.fold (\l -> return . (:l)) [] . STM.stream
+stmMapToList :: STMMap.Map k v -> STM [(k, v)]
+stmMapToList = ListT.fold (\l -> return . (:l)) [] . STMMap.stream
 
-createNewLayer :: Layer.LayerRequestList -> App.ActionHandler S.NoContent
+createNewLayer :: Layer.LayerRequestList -> App.ActionHandler Servant.NoContent
 createNewLayer (Layer.LayerRequestList layerRequests) =
-  newLayer (\lastModifiedTime ls -> mapM_ (\lr -> STM.insert (Layer.requestToLayer (Layer._newLayerRequestName lr) (Layer._newLayerRequestSettings lr) lastModifiedTime) (Layer._newLayerRequestName lr) ls) layerRequests)
+  newLayer (\lastModifiedTime ls -> mapM_ (\lr -> STMMap.insert (Layer.requestToLayer (Layer._newLayerRequestName lr) (Layer._newLayerRequestSettings lr) lastModifiedTime) (Layer._newLayerRequestName lr) ls) layerRequests)
 
-provisionLayer :: T.Text -> Layer.LayerSettings -> App.ActionHandler S.NoContent
+provisionLayer :: T.Text -> Layer.LayerSettings -> App.ActionHandler Servant.NoContent
 provisionLayer l settings =
-  newLayer (\lastModifiedTime -> STM.insert (Layer.requestToLayer l settings lastModifiedTime) l)
+  newLayer (\lastModifiedTime -> STMMap.insert (Layer.requestToLayer l settings lastModifiedTime) l)
 
-newLayer :: (MonadIO m, RC.MonadReader App.ServerState m) => (UTCTime -> STM.Map T.Text Layer.Layer -> STM a) -> m S.NoContent
+newLayer :: (MonadIO m, RC.MonadReader App.ServerState m) => (UTCTime -> STMMap.Map T.Text Layer.Layer -> STM a) -> m Servant.NoContent
 newLayer b = do
   r <- RC.ask
   lastModifiedTime <- liftIO getCurrentTime
@@ -65,26 +65,26 @@ newLayer b = do
     stmMapToList ls
   let newNewLayers = fmap (\(k, v) -> (k, Layer.layerToLayerDetails v)) newLayers
   liftIO $ LBS8.writeFile cfgFile (AE.encodePretty (originalCfg {Config._configLayers = fromList newNewLayers}))
-  pure S.NoContent
+  pure Servant.NoContent
 
 getQuery :: T.Text -> Natural -> Natural -> Natural -> App.ActionHandler T.Text
 getQuery l z x y = do
   layer <- getLayerOrThrow l
-  DB.mkQuery layer z (x, y)
+  DBLayer.mkQuery layer z (x, y)
 
-getContent :: T.Text -> Natural -> Natural -> T.Text -> Maybe T.Text -> App.ActionHandler (S.Headers '[S.Header "Last-Modified" T.Text] BS.ByteString)
+getContent :: T.Text -> Natural -> Natural -> T.Text -> Maybe T.Text -> App.ActionHandler (Servant.Headers '[Servant.Header "Last-Modified" T.Text] BS.ByteString)
 getContent l z x stringY maybeIfModified =
   do
     layer <- getLayerOrThrow l
-    if DB.isModified layer maybeIfModified
+    if DBLayer.isModified layer maybeIfModified
       then getContent' layer z x stringY
-      else throwError S.err304
+      else throwError Servant.err304
 
-getContent' :: Layer.Layer -> Natural -> Natural -> T.Text -> App.ActionHandler (S.Headers '[S.Header "Last-Modified" T.Text] BS.ByteString)
+getContent' :: Layer.Layer -> Natural -> Natural -> T.Text -> App.ActionHandler (Servant.Headers '[Servant.Header "Last-Modified" T.Text] BS.ByteString)
 getContent' l z x stringY
   | ".mvt" `T.isSuffixOf` stringY = getAnything getTile l z x stringY
   | ".json" `T.isSuffixOf` stringY = getAnything getJson l z x stringY
-  | otherwise = throwError $ S.err400 { S.errBody = "Unknown request: " <> LBS8.fromStrict (TE.encodeUtf8 stringY) }
+  | otherwise = throwError $ Servant.err400 { Servant.errBody = "Unknown request: " <> LBS8.fromStrict (TE.encodeUtf8 stringY) }
 
 getAnything :: (t -> DGTT.ZoomLevel -> (DGTT.Pixels, DGTT.Pixels) -> App.ActionHandler a) -> t -> DGTT.ZoomLevel -> DGTT.Pixels -> T.Text -> App.ActionHandler a
 getAnything f layer z x stringY =
@@ -94,7 +94,7 @@ getAnything f layer z x stringY =
   where
     getY s = DTR.decimal $ T.takeWhile C.isNumber s
 
-getTile :: Layer.Layer -> DGTT.ZoomLevel -> (DGTT.Pixels, DGTT.Pixels) -> App.ActionHandler (S.Headers '[S.Header "Last-Modified" T.Text] BS.ByteString)
+getTile :: Layer.Layer -> DGTT.ZoomLevel -> (DGTT.Pixels, DGTT.Pixels) -> App.ActionHandler (Servant.Headers '[Servant.Header "Last-Modified" T.Text] BS.ByteString)
 getTile layer z xy = do
   geoJson <- getJson' layer z xy
   buffer  <- RC.asks (^. App.ssBuffer)
@@ -102,24 +102,24 @@ getTile layer z xy = do
   tile    <- liftIO $ Tile.mkTile (Layer._layerName layer) z xy buffer (Layer._layerQuantize layer) simplificationAlgorithm geoJson
   checkEmpty tile layer
 
-checkEmpty :: BS.ByteString -> Layer.Layer -> App.ActionHandler (S.Headers '[S.Header "Last-Modified" T.Text] BS.ByteString)
+checkEmpty :: BS.ByteString -> Layer.Layer -> App.ActionHandler (Servant.Headers '[Servant.Header "Last-Modified" T.Text] BS.ByteString)
 checkEmpty tile layer
-  | BS.null tile = throwError $ App.err204 { S.errHeaders = [(hLastModified, TE.encodeUtf8 $ DB.lastModified layer)] }
-  | otherwise = pure $ S.addHeader (DB.lastModified layer) tile
+  | BS.null tile = throwError $ App.err204 { Servant.errHeaders = [(hLastModified, TE.encodeUtf8 $ DBLayer.lastModified layer)] }
+  | otherwise = pure $ Servant.addHeader (DBLayer.lastModified layer) tile
 
-getJson :: Layer.Layer -> DGTT.ZoomLevel -> (DGTT.Pixels, DGTT.Pixels) -> App.ActionHandler (S.Headers '[S.Header "Last-Modified" T.Text] BS.ByteString)
-getJson layer z xy = S.addHeader (DB.lastModified layer) . LBS8.toStrict . A.encode <$> getJson' layer z xy
+getJson :: Layer.Layer -> DGTT.ZoomLevel -> (DGTT.Pixels, DGTT.Pixels) -> App.ActionHandler (Servant.Headers '[Servant.Header "Last-Modified" T.Text] BS.ByteString)
+getJson layer z xy = Servant.addHeader (DBLayer.lastModified layer) . LBS8.toStrict . A.encode <$> getJson' layer z xy
 
 getJson' :: Layer.Layer -> DGTT.ZoomLevel -> (DGTT.Pixels, DGTT.Pixels) -> App.ActionHandler (DG.GeoFeatureCollection A.Value)
 getJson' layer z xy = do
-  errorOrTfs <- DB.findFeatures layer z xy
+  errorOrTfs <- DBLayer.findFeatures layer z xy
   case errorOrTfs of
-    Left e    -> throwError $ S.err500 { S.errBody = LBS8.pack $ show e }
+    Left e    -> throwError $ Servant.err500 { Servant.errBody = LBS8.pack $ show e }
     Right tfs -> pure $ DG.GeoFeatureCollection Nothing (Layer.mkGeoJSON tfs)
 
 getLayerOrThrow :: T.Text -> App.ActionHandler Layer.Layer
 getLayerOrThrow l = do
-  errorOrLayer <- DB.getLayer l
+  errorOrLayer <- DBLayer.getLayer l
   case errorOrLayer of
-    Left DB.LayerNotFound -> throwError $ S.err404 { S.errBody = "Layer not found :-(" }
+    Left DBLayer.LayerNotFound -> throwError $ Servant.err404 { Servant.errBody = "Layer not found :-(" }
     Right layer -> pure layer
