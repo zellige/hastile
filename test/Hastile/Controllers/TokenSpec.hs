355e0291d20501a90a6611aca12635cf10e721e4
Dominic Endrei <dominic.endrei@gmail.com>
2018-08-21 15:46:52 +1000

    More token tests.

--- test/Hastile/Controllers/TokenSpec.hs
+++ test/Hastile/Controllers/TokenSpec.hs
@@ -2,12 +2,13 @@
 
 module Hastile.Controllers.TokenSpec where
 
-import qualified Data.IORef          as IORef
-import qualified Data.LruCache       as LRU
-import qualified Data.LruCache.IO    as LRUIO
-import           Test.Hspec          (Spec, describe, it, runIO, shouldBe)
+import qualified Data.IORef                as IORef
+import qualified Data.LruCache             as LRU
+import qualified Data.LruCache.IO          as LRUIO
+import           Test.Hspec                (Spec, describe, it, runIO, shouldBe)
 
-import qualified Hastile.Types.Token as Token
+import qualified Hastile.Controllers.Token as Token
+import qualified Hastile.Types.Token       as Token
 
 spec :: Spec
 spec =
@@ -17,18 +18,31 @@ testCache :: Spec
 testCache =
   describe "testCache" $ do
     cache <- runIO $ LRUIO.newLruHandle 1
-    _ <- runIO $ LRUIO.cached cache sampleToken (pure sampleLayers)
-    it "should insert a token into the cache" $ do
-      let (LRUIO.LruHandle ref) = cache
-      unwrappedCache <- IORef.readIORef ref
-      case LRU.lookup sampleToken unwrappedCache of
-        Just (foundLayers, _) ->
-          foundLayers `shouldBe` sampleLayers
-        Nothing ->
-          fail "did not find token"
+    _ <- runIO $ LRUIO.cached cache exampleToken (pure exampleLayers)
+    it "should insert a token into the cache" $
+      checkCache cache exampleToken exampleLayers
+    it "should update the authorised layers of a token in the cache" $ do
+      let updatedToken = Token.TokenAuthorisation exampleToken updatedExampleLayers
+      Token.updateCache cache updatedToken
+      checkCache cache exampleToken updatedExampleLayers
+    it "should deauthorise a token in the cache" $ do
+      let updatedToken = Token.unauthorisedToken exampleToken
+      Token.updateCache cache updatedToken
+      checkCache cache exampleToken []
+  where checkCache cache token expectedLayers = do
+          let (LRUIO.LruHandle ref) = cache
+          unwrappedCache <- IORef.readIORef ref
+          case LRU.lookup token unwrappedCache of
+            Just (foundLayers, _) ->
+              foundLayers `shouldBe` expectedLayers
+            Nothing ->
+              fail "did not find token"
 
-sampleToken :: Token.Token
-sampleToken = "abcd"
+exampleToken :: Token.Token
+exampleToken = "abcd"
 
-sampleLayers :: Token.Layers
-sampleLayers = ["layer1", "layer2"]
+exampleLayers :: Token.Layers
+exampleLayers = ["layer1", "layer2"]
+
+updatedExampleLayers :: Token.Layers
+updatedExampleLayers = ["layer1", "layer3"]
