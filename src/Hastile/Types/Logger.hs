cb2fa9e80810e2eaccfdef26bc8c32f9c4a952a9
newmana <andrewfnewman@gmail.com>
2019-02-19 16:28:04 +1000

    .

--- src/Hastile/Types/Logger.hs
+++ src/Hastile/Types/Logger.hs
@@ -6,22 +6,22 @@ import qualified Katip
 import qualified System.IO             as IO
 import qualified System.Log.FastLogger as FastLogger
 
-fromLevel :: LogLevel -> Severity
-fromLevel LevelDebug     = DebugS
-fromLevel LevelInfo      = InfoS
-fromLevel LevelWarn      = WarningS
-fromLevel LevelError     = ErrorS
-fromLevel (LevelOther _) = NoticeS
+fromLevel :: Logger.LogLevel -> Katip.Severity
+fromLevel Logger.LevelDebug     = Katip.DebugS
+fromLevel Logger.LevelInfo      = Katip.InfoS
+fromLevel Logger.LevelWarn      = Katip.WarningS
+fromLevel Logger.LevelError     = Katip.ErrorS
+fromLevel (Logger.LevelOther _) = Katip.NoticeS
 
-defaultLogEnv :: IO LogEnv
+defaultLogEnv :: IO Katip.LogEnv
 defaultLogEnv = do
-    handleScribe <- mkHandleScribe ColorIfTerminal IO.stdout DebugS V2
-    env <- initLogEnv "hastile" "production"
-    registerScribe "stdout" handleScribe defaultScribeSettings env
+    handleScribe <- Katip.mkHandleScribe Katip.ColorIfTerminal IO.stdout Katip.DebugS Katip.V2
+    env <- Katip.initLogEnv "hastile" "production"
+    Katip.registerScribe "stdout" handleScribe Katip.defaultScribeSettings env
 
-adapt :: (ToLogStr msg, Applicative m, Katip m)  => (Namespace -> Severity -> Katip.LogStr -> m ()) -> Loc -> LogSource -> LogLevel -> msg -> m ()
+adapt :: (FastLogger.ToLogStr msg, Applicative m, Katip.Katip m)  => (Katip.Namespace -> Katip.Severity -> Katip.LogStr -> m ()) -> Logger.Loc -> Logger.LogSource -> Logger.LogLevel -> msg -> m ()
 adapt f _ src lvl msg =
     f ns (fromLevel lvl) $ logStr' msg
   where
-    ns = Namespace [src]
+    ns = Katip.Namespace [src]
 logStr' = Katip.logStr . FastLogger.fromLogStr . Logger.toLogStr
