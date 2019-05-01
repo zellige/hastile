467758c132492a1be598f972711fdbfbd62fc26c
Dominic Endrei <dominic.endrei@gmail.com>
2019-05-01 10:17:47 +1000

    Log errors

--- src/Hastile/Config.hs
+++ src/Hastile/Config.hs
@@ -4,18 +4,22 @@
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE RecordWildCards       #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeOperators         #-}
 
 module Hastile.Config where
 
+import qualified Control.Exception.Base     as ControlException
 import qualified Data.Aeson                 as Aeson
 import qualified Data.ByteString.Lazy.Char8 as ByteStringLazyChar8
 import qualified Data.Either                as DataEither
+import qualified Data.List                  as DataList
 import qualified Data.Map.Strict            as DataMapStrict
 import qualified Data.Maybe                 as DataMaybe
 import qualified Data.Text.Encoding         as TextEncoding
 import qualified Hasql.Pool                 as HasqlPool
-import           System.Exit                as SystemExit
+import qualified Katip
+import qualified System.Exit                as SystemExit
 
 import qualified Hastile.Lib.Layer          as LibLayer
 import qualified Hastile.Types.Config       as Config
@@ -26,7 +30,7 @@ getConfig cfgFile = do
   case Aeson.eitherDecode configBs of
     Left e -> do
       putStrLn $ "In file: " <> cfgFile <> "\nError: " <> e
-      exitWith (SystemExit.ExitFailure 2)
+      SystemExit.exitWith (SystemExit.ExitFailure 2)
     Right config -> pure (addDefaults config)
 
 addDefaults :: Config.InputConfig -> Config.Config
@@ -43,15 +47,22 @@ addDefaults Config.InputConfig{..} =
     _inputConfigLayers
     (DataMaybe.fromMaybe 128 _inputConfigTileBuffer)
 
-checkConfig :: FilePath -> Config.Config -> IO ()
-checkConfig cfgFile Config.Config{..} = do
+checkConfig :: Katip.LogEnv -> FilePath -> Config.Config -> IO ()
+checkConfig logEnv cfgFile Config.Config{..} = do
   pool <- HasqlPool.acquire (_configPgPoolSize, _configPgTimeout, TextEncoding.encodeUtf8 _configPgConnection)
   let layers = DataMapStrict.elems _configLayers
   result <- mapM (LibLayer.checkLayerExists pool) layers
   case DataEither.lefts result of
     [] ->
       pure ()
-    errs -> do
-      putStrLn $ "In file: " <> cfgFile <> "\nError: "
-      mapM_ (\err -> putStrLn $ "  " <> err) errs
+    errs ->
+      ControlException.bracket (pure logEnv) (\_ -> pure ()) $ \le ->
+        Katip.runKatipContextT le (mempty :: Katip.LogContexts) mempty (logErrors cfgFile errs)
   HasqlPool.release pool
+
+logErrors :: FilePath -> [String] -> Katip.KatipContextT IO ()
+logErrors cfgFile errs =
+  $(Katip.logTM) Katip.ErrorS $ Katip.logStr $
+    "Error in file: " <> cfgFile <> separator <>
+      DataList.concat (DataList.intersperse separator errs)
+  where separator = "\n  "
