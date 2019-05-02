c6297eff735d98b61446bd60e5124a13fa9fa3f2
Dominic Endrei <dominic.endrei@gmail.com>
2019-05-02 16:23:49 +1000

    Only create server with token api if in authenticated mode

--- src/Hastile/Controllers.hs
+++ src/Hastile/Controllers.hs
@@ -18,8 +18,12 @@ import qualified Hastile.Routes             as Routes
 import qualified Hastile.Types.App          as App
 import qualified Hastile.Types.Config       as Config
 
-hastileServer :: (MonadIO.MonadIO m) => Servant.ServerT Routes.HastileApi (App.ActionHandler m)
-hastileServer = returnConfiguration
+publicHastileServer :: (MonadIO.MonadIO m) => Servant.ServerT Routes.PublicHastileApi (App.ActionHandler m)
+publicHastileServer = returnConfiguration
+  Servant.:<|> Layer.layerServer
+
+authenticatedHastileServer :: (MonadIO.MonadIO m) => Servant.ServerT Routes.AuthenticatedHastileApi (App.ActionHandler m)
+authenticatedHastileServer = returnConfiguration
   Servant.:<|> Token.tokenServer
   Servant.:<|> Layer.layerServer
 
